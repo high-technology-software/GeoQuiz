c3fe43382732ea3bbe653af1f65799d761568add
foxtrot-developer <albertmotosgonzalez@gmail.com>
2015-12-06 22:38:02 +0100

    Tested attempt

--- app/src/main/java/com/high_technology_software/concept/geoquiz/QuizActivity.java
+++ app/src/main/java/com/high_technology_software/concept/geoquiz/QuizActivity.java
@@ -13,6 +13,7 @@ public class QuizActivity extends Activity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
     private TrueFalse[] mQuestionBank = new TrueFalse[] {
@@ -73,6 +74,15 @@ public class QuizActivity extends Activity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mPrevButton = (Button)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex - 1 + mQuestionBank.length) % mQuestionBank.length;
                 updateQuestion();
             }
         });
